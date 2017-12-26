3b34553052a9947856e02ff3d40c43ff75dfe84c
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-12-26 03:22:23 -0200

    add rom title read

--- src/main.c
+++ src/main.c
@@ -1,9 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "rom.h"
 
 
 int main(int argc, char** argv)
 {
+	if (argc < 2) {
+		fprintf(stderr, "Usage: %s [romfile]\n", argv[0]);
+		return EXIT_FAILURE;
+	}
 
-	return 0;
+	FILE* const file = fopen(argv[1], "r");
+	if (file == NULL) {
+		perror("Couldn't open file: ");
+		return EXIT_FAILURE;
+	}
+
+	fseek(file, 0, SEEK_END);
+	const long size = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	
+	uint8_t* const data = malloc(size);
+
+	if (data == NULL) {
+		perror("Couldn't allocate memory: ");
+		goto Lfclose;
+	}
+
+	if (fread(data, 1, size, file) < (unsigned long)size) {
+		perror("Couldn't read file: ");
+		goto Lfreedata;
+	}
+
+	loadrom(data);
+	freerom();
+Lfreedata:
+	free(data);
+Lfclose:
+	fclose(file);
+	return EXIT_FAILURE;
 }
 
--- src/rom.c
+++ src/rom.c
@@ -1,13 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "rom.h"
+
+
+static char romname[16] = { '\0' };
 
 
 bool loadrom(const uint8_t* const data)
 {
+	if (data == NULL)
+		return false;
 
+	memcpy(romname, &data[0x134], 15);
+	printf("ROM NAME: %s\n", romname);
+	return true;
 }
 
 void freerom(void)
 {
-
+	memset(romname, 0, 16);
 }
 
