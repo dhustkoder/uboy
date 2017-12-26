60c80ec266ac958d0a2585ca47bd517cf4d80fa2
Rafael Moura <rafaelmoura.dev@gmail.com>
2017-12-26 11:04:56 -0200

    added more rom info

--- src/rom.c
+++ src/rom.c
@@ -6,6 +6,11 @@
 
 static char romname[16] = { '\0' };
 static uint8_t cgbflag = 0;
+static uint8_t sgbflag = 0;
+static uint8_t romtype = 0;
+static uint16_t rom_nbanks = 0;
+static uint32_t romsize = 0;
+
 
 bool loadrom(const uint8_t* const data)
 {
@@ -14,16 +19,30 @@ bool loadrom(const uint8_t* const data)
 
 	memcpy(romname, &data[0x134], 15);
 	cgbflag = data[0x143];
+	sgbflag = data[0x146];
+	romtype = data[0x147];
 
-	printf("ROM NAME: %s\n"
-	       "CGB Flag: $%X\n",
-	       romname, cgbflag);
+	const uint16_t banks1[9] = { 2, 4, 16, 32, 64, 128, 256, 512 };
+	const uint8_t banks2[3] = { 72, 80, 96 };
+	rom_nbanks = data[0x148] <= 0x08 ? banks1[data[0x148]]
+	                                 : banks2[data[0x148] - 0x52];
+	romsize = rom_nbanks * 0x4000;
+
+	printf("Rom Name: %s\n"
+	       "CGB Flag: $%X\n"
+	       "SGB Flag: $%X\n"
+	       "Rom Type: $%X\n"
+	       "Rom N Banks: $%X\n"
+	       "Rom Size: $%X\n",
+	       romname, cgbflag,
+	       sgbflag, romtype,
+	       rom_nbanks, romsize);
+
 
 	return true;
 }
 
 void freerom(void)
 {
-	memset(romname, 0, 16);
 }
 
